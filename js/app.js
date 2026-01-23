069d945b3cfa5f8ab441be83e0c54f6aad84f048
ProduccionesvGv <gabrielvitalis34@gmail.com>
2026-01-23 15:11:21 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -242,7 +242,7 @@ try{
       }
 
       // WhatsApp destination (E.164 without '+')
-      const phone = '+5491158768929';
+      const phone = '5491158768929';
       let msg = '';
       if(folder === '02Genext' && id === '01'){
         msg = 'Hola NOVA Seeds, quisiera comprar Dealer Deal XXL AutoMix (x12 semillas). ¿Hay stock disponible?';
